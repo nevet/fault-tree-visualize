768ee5a333ea507def717bfacb7a67576a332fa2
Huang Da <nevets.st@gmail.com>
2016-03-13 01:39:16 +0800

    basic backend logic done, need more tests but who cares

--- controller/controller.js
+++ controller/controller.js
@@ -1,1 +1,62 @@
-// TODO: controller of the app
+(function (controller, $, undefined) {
+  controller.faultTree = {}
+
+  var stack = []
+
+  function loadData() {
+    $.getJSON("data/mock.json", function (data) {
+      controller.faultTree = data
+      view.updateTree(getNextLayer(""), false);
+    }).fail(function(jqxhr, status, error) {
+      var err = status + ", " + error;
+      console.log("Request Failed: " + err);
+    })
+  }
+
+  function getCurrentObj() {
+    var obj = controller.faultTree
+    $.each(stack, function(i, val) {
+      obj = obj[stack[i]]
+    });
+
+    return obj
+  }
+
+  function getNextLayer(key) {
+    var nextLayer = [];
+    var curObj = getCurrentObj();
+    var nextObj = curObj;
+
+    if (key) {
+      nextObj = curObj[key];
+      stack.push(key);
+    }
+
+    $.each(nextObj, function(i, val) {
+      if (i == "desp") {
+        nextLayer.push({"content": val, "hasNext": false});
+      } else {
+        nextLayer.push({"content": i, "hasNext": true});
+      }
+    });
+
+    return nextLayer;
+  }
+
+  $(function () {
+    loadData();
+  });
+
+  $("ul").on("click", "li."+view.nodeName, function(event) {
+    clicked = $(event.target).html();
+    view.updateTree(getNextLayer(clicked), true);
+
+    event.preventDefault();
+  });
+
+  $("ul").on("click", "li."+view.navName, function(event) {
+    stack.pop()
+    view.updateTree(getNextLayer(stack.pop()), stack.length >= 1);
+    event.preventDefault();
+  });
+} (window.controller = window.controller || {}, jQuery));
--- view/view.js
+++ view/view.js
@@ -1,1 +1,38 @@
-// TODO: view of the app
+(function (view, $, undefined) {
+  view.nodeName = "fault_node"
+  view.leafName = "fault_leaf"
+  view.navName = "item_nav"
+
+  var body = $("body")
+  var canvas = $("#canvas")
+  var itemNav = $("#canvas ."+view.navName)
+
+  view.updateTree = function(data, needNav) {
+    removeOld();
+
+    if (needNav) {
+      itemNav.addClass("clickable");
+    } else {
+      itemNav.removeClass("clickable");
+    }
+
+    $.each(data, function(i, val) {
+      content = val["content"];
+      node = $("<li></li>");
+
+      if (val["hasNext"]) {
+        node.addClass(view.nodeName);
+        node.addClass("clickable");
+      } else {
+        node.addClass(view.leafName);
+      }
+
+      node.html(content);
+      node.appendTo(canvas);
+    });
+  }
+
+  function removeOld() {
+    $("#canvas li").remove("."+view.nodeName + ",." + view.leafName)
+  }
+} (window.view = window.view || {}, jQuery));
